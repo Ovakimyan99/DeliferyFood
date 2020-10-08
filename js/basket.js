2f12df6bdac05e476c86f14334712ba0c8c9bd97
Tigran <tigr.kman@gmail.com>
2020-10-08 17:57:04 +0300

    Полностью готов адаптив. Настроена верстка модалки. Нет прописанных скриптов

---
+++ js/basket.js
@@ -0,0 +1,15 @@
+// 'use strict';
+
+// const basket = document.querySelector('[data-basket]'),
+//       modalWrapper = document.createElement('div'),
+//       modal = modalWrapper.createElement('div'),
+//       body = document.body;
+
+    
+// // Create modal
+
+// modalWrapper.classList.add('modal-wrapper');
+// modal.classList.add('')
+
+
+// body.append(modalWrapper);
